847de44c9b9c24414d737060aeeaf64fcddd6124
Ischozar2 <alexander.hollmann@gmx.at>
2017-06-02 21:53:29 +0200

    Added Maps!

---
+++ GUI/Converter/GeoCoordinateToLocationConverter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GUI.Converter
+{
+   public class GeoCoordinateToLocationConverter : IValueConverter    
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+        if(value is GeoCoordinate)
+            {
+                GeoCoordinate gc = (GeoCoordinate)value;
+                return new Location(gc.Latitude,gc.Longitude);
+            }
+            return null;
+         
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
